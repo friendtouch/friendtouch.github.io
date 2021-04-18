b6e395c413e41743247967f8658af882fbb82a00
gnehs <jayuiop@gmail.com>
2021-04-18 23:44:49 +0800

    added footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,15 @@
 	<v-app dark style="background: transparent">
 		<div class="neon-title neon-text neon-shadow">Friend Touch</div>
 		<nuxt />
+		<v-divider />
+		<footer class="mt-1">Designed in Taiwan</footer>
 	</v-app>
 </template>
-
+<style lang="sass">
+footer
+	opacity: .7
+	font-size: 12px
+</style>
 <script>
 export default {
 	data() {
