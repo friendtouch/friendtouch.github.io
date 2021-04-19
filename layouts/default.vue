28d243420b28a2d2d1b1faf0be0ac406c5bca156
gnehs <jayuiop@gmail.com>
2021-04-19 09:28:07 +0800

    added social link

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,24 @@
 		<div class="neon-title neon-text neon-shadow">Friend Touch</div>
 		<nuxt />
 		<v-divider />
-		<footer class="mt-1">Designed in Taiwan</footer>
+		<footer class="mt-1 d-flex px-2 align-center">
+			<div class="flex-grow-1">Designed in Taiwan</div>
+			<div>
+				<v-btn icon color="white" href="https://www.instagram.com/friend.touch/" target="_blank">
+					<v-icon>fab fa-instagram</v-icon>
+				</v-btn>
+				<v-btn icon color="white" href="https://www.facebook.com/Friend.Touch.Taiwan/" target="_blank">
+					<v-icon>fab fa-facebook</v-icon>
+				</v-btn>
+			</div>
+		</footer>
 	</v-app>
 </template>
 <style lang="sass">
 footer
 	opacity: .7
-	font-size: 12px
+	font-size: 14px
+	text-align: left
 </style>
 <script>
 export default {
